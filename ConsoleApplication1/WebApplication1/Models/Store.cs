449b73a3ae53bf533ad3a0783784942ab340a940
KingMido <dj_sweden_20@hotmail.com>
2016-05-27 11:08:56 +0200

    .

--- ConsoleApplication1/WebApplication1/Models/Store.cs
+++ ConsoleApplication1/WebApplication1/Models/Store.cs
@@ -9,6 +9,8 @@ namespace WebApplication1.Models
     public class Store
     {
         public int Id { get; set; }
+        [Maxlength(1)]
+        //validation sätts här övanför metoden, jag har missat vad man ska göra för de skulle fungera.
         public string Name { get; set; }
         public string Address { get; set; }
         public string Products { get; set; }
